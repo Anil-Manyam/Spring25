27a564443d26d2b95c8cf250469cda494e2317c8
Anil-Manyam <anilmanyam.630@gmail.com>
2025-07-18 16:32:43 -0230

    documentation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,12 +18,12 @@ export default function App() {
         <Route path="/" element={<WelcomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/home" element={<ProfilePage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/welcome" element={<WelcomePage />} />
         <Route path="/books" element={<BookListPage />} />
         <Route path="/fines" element={<FinesPage />} />
-        <Route path="/feedback" element={<FeedbackPage />} />
+        {/* <Route path="/feedback" element={<FeedbackPage />} /> */}
         <Route path="/admin/users" element={<UserManagementPage />} />
         <Route path="/admin/books" element={<BookManagementPage />} />
       </Routes>
